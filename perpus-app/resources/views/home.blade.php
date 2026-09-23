40ebf8d333e27a237a7039818b32e4ddfe23616b
M. Rizky Firdaus <rizkyfirdaus07@gmail.com>
2026-09-23 17:19:20 +0700

    Pertemuan 3 - Routing, Controller, dan Blade dasar

---
+++ perpus-app/resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app') 
+ 
+@section('title', $judul) 
+ 
+@section('content') 
+    <h1>Selamat datang di {{ $judul }}</h1> 
+    <p>Ini adalah halaman utama aplikasi yang sedang dibangun pada mata kuliah Pemrograman Web II.</p> @endsection 
---
+++ perpus-app/resources/views/kontak.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Kontak</title>
+</head>
+<body>
+
+    <h1>Halaman Kontak</h1>
+
+    <p>Silakan hubungi kami melalui informasi berikut:</p>
+
+    <p><strong>Nama:</strong> Perpustakaan Mini</p>
+    <p><strong>Email:</strong> perpustakaanmini@gmail.com</p>
+    <p><strong>No. HP:</strong> 0812-3456-7890</p>
+    <p><strong>Alamat:</strong> batusangkar, Sumatera Barat</p>
+
+</body>
+</html>
